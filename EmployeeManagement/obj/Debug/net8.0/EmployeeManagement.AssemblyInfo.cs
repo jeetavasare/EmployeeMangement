9c4a449d3b8099b8f9715ef42a6bf1a9e7a70a55
Jeet-exe <jeetavasare@gmail.com>
2024-06-05 19:59:03 +0530

    second gitignore

--- EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e278e561ca2f602a8865c5a2bbc4d1f40a32b28f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
