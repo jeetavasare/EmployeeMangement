f5db383f39b30319e3d5f73498bbb6698093d0c3
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-06-09 23:52:52 +0530

    Added Logging Capabilites - Added NLog to log to file - File Logs will be the path given in nlog.config file - Info Log will be generated when an Employee is Added,Updated or Deleted as implemented in SQLEmployeeRepository - Logs will be generated if any status code error (for now only 404) and any exception will also be logged -> implemented in ErrorController - Stack trace for 404 and exception will not be shown in Error.cshtml view and NotFound.cshtml Views

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -6,7 +6,14 @@ namespace EmployeeManagement.Controllers
 {
 	public class ErrorController : Controller
 	{
-		[Route("Error/{statusCode}")]
+		private readonly ILogger<ErrorController> logger;
+
+		public ErrorController(ILogger<ErrorController> logger)
+        {
+			this.logger = logger;
+		}
+
+        [Route("Error/{statusCode}")]
 		public IActionResult HttpStatusCodeHandler(int statusCode)
 		{
 			switch (statusCode)
@@ -16,6 +23,10 @@ namespace EmployeeManagement.Controllers
 					ViewBag.ErrorMessage = "The resource you are looking for could not be found";
 					ViewBag.URL = statusCodeResult.OriginalPath;
 					ViewBag.QS = statusCodeResult.OriginalQueryString;
+
+					logger.LogWarning($"404 Error Occured. Path: {statusCodeResult.OriginalPath}" +
+						$"\nQuery String: {statusCodeResult.OriginalQueryString}");
+
 					break;
 			}
 
@@ -27,9 +38,11 @@ namespace EmployeeManagement.Controllers
 		public IActionResult Error()
 		{
 			var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerFeature>();
-			ViewBag.exceptionPath = exceptionDetails.Path;
+			//ViewBag.exceptionPath = exceptionDetails.Path;
 			ViewBag.exceptionMessage = exceptionDetails.Error.Message;
-			ViewBag.stackTrace = exceptionDetails.Error.StackTrace;
+			//ViewBag.stackTrace = exceptionDetails.Error.StackTrace;
+
+			logger.LogError($"The path {exceptionDetails.Path} threw {exceptionDetails.Error.Message}");
 
 			return View();
 		}
