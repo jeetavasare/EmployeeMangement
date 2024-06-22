05a44431d6f348d34853ce437809b40299f5504d
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-06-22 13:49:04 +0530

    Added Delete Confirmation for Registered users when deleted by the adminsitrator. Part 87

--- EmployeeManagement/wwwroot/js/CustomScript.js
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -1,2 +1,16 @@
 ﻿console.log("lob");
 document.getElementsByTagName("div")[1].style.color = "Red";
+
+function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    }
+    else {
+        $('#' + confirmDeleteSpan).hide();
+        $('#' + deleteSpan).show();
+    }
+}
