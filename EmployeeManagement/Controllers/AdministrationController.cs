8ca450b20fb87a7d353b29d51e29be2620c17681
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-06-26 14:38:44 +0530

    Added user claims to support claim based authorization

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace EmployeeManagement.Controllers
 {
@@ -388,5 +389,82 @@ namespace EmployeeManagement.Controllers
 			return RedirectToAction("EditUsers", new { Id = id });
 		}
 
-	}
+
+		[HttpGet]
+		public async Task<IActionResult> ManageUserClaims(string userId)
+		{
+			var user = await userManager.FindByIdAsync(userId);
+
+			if (user == null)
+			{
+				ViewBag.ErrorMessage = $"User with Id = {userId} cannot be found";
+				return View("NotFound");
+			}
+
+			// UserManager service GetClaimsAsync method gets all the current claims of the user
+			var existingUserClaims = await userManager.GetClaimsAsync(user);
+
+			var model = new UserClaimsViewModel
+			{
+				UserId = userId
+			};
+
+			// Loop through each claim we have in our application
+			foreach (Claim claim in ClaimsStore.AllClaims)
+			{
+				UserClaim userClaim = new UserClaim
+				{
+					ClaimType = claim.Type
+				};
+
+				// If the user has the claim, set IsSelected property to true, so the checkbox
+				// next to the claim is checked on the UI
+				if (existingUserClaims.Any(c => c.Type == claim.Type))
+				{
+					userClaim.IsSelected = true;
+				}
+
+				model.Claims.Add(userClaim);
+			}
+
+			return View(model);
+
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> ManageUserClaims(UserClaimsViewModel model)
+        {
+            var user = await userManager.FindByIdAsync(model.UserId);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {model.UserId} cannot be found";
+                return View("NotFound");
+            }
+
+            // Get all the user existing claims and delete them
+            var claims = await userManager.GetClaimsAsync(user);
+            var result = await userManager.RemoveClaimsAsync(user, claims);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot remove user existing claims");
+                return View(model);
+            }
+
+            // Add all the claims that are selected on the UI
+            result = await userManager.AddClaimsAsync(user,
+                model.Claims.Where(c => c.IsSelected).Select(c => new Claim(c.ClaimType, c.ClaimType)));
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected claims to user");
+                return View(model);
+            }
+
+            return RedirectToAction("EditUsers", new { Id = model.UserId });
+
+        }
+
+    }
 }
