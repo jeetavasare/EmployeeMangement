4a407910abe56bae1b45107197241de34e348180
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-07-06 14:15:03 +0530

    Password reset link expires after 5 hrs Email Confirmation link expires after 3 days

---
+++ EmployeeManagement/Security/CustomEmailConfirmationTokenProvider.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Identity;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace EmployeeManagement.Security
+{
+    public class CustomEmailConfirmationTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomEmailConfirmationTokenProvider(IDataProtectionProvider dataProtectionProvider, IOptions<CustomEmailConfirmationTokenProviderOptions> options, ILogger<DataProtectorTokenProvider<TUser>> logger)
+            : base(dataProtectionProvider,options,logger)
+        {
+            
+        }
+    }
+}
---
+++ EmployeeManagement/Security/CustomEmailConfirmationTokenProviderOptions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmployeeManagement.Security
+{
+    public class CustomEmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+
+    }
+}
