90f9e5b660d8859d1686852d449f2275a1110fff
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-07-06 18:44:01 +0530

    Now users can change their passwords

---
+++ EmployeeManagement/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string CurrentPassword { get; set; }
+    
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("NewPassword", ErrorMessage = "Current Password and confirm passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
