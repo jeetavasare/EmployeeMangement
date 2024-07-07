1183d399dc0622598da963d67467409f68f2a7ba
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-07-07 11:47:20 +0530

    Now users who have registered through external providers can set their local password and sign in via their password as well

---
+++ EmployeeManagement/ViewModels/SetPasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class SetPasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
